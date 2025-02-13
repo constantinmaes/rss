5b6ac2612d37381ad7a81c37f472be94c5294933
Constantin Maes <constantinmaes@gmail.com>
2025-02-13 20:43:18 +0100

    Add new files and update package.json to include pug dependency

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const parser = new XMLParser();
 const app = express();
 const port = 3000;
 
+// Utiliser pug
+app.set('view engine', 'pug');
+
 // 1. Récupérer le fichier XML
 // 2. Parser le fichier XML
 // 3. Afficher les données
@@ -14,13 +17,19 @@ app.get('/', async (req, res) => {
     const xml = await axios.get(
         'https://rss.rtbf.be/article/rss/highlight_rtbf_info.xml?source=internal'
     );
-    console.log('XML', xml.data);
     const parsed = parser.parse(xml.data);
+    const title = parsed.rss.channel.title;
+    const pubDate = parsed.rss.channel.pubDate;
+    const articles = parsed.rss.channel.item;
     console.log('DONE', new Date());
+    console.log(title, pubDate, articles.length);
     // res.send('Hello');
-    res.status(200).json({
-        rss: parsed,
-    });
+    /*res.status(200).json({
+        title,
+        pubDate,
+        articles,
+    });*/
+    res.render('index', { title, pubDate, articles });
 });
 
 app.listen(port, () => {
