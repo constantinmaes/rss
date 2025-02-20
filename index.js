541814dd71fd3cfae36c99414f598028091cac56
Constantin Maes <constantinmaes@gmail.com>
2025-02-20 18:31:40 +0100

    Add node-cron dependency and UUID utility files; update package.json

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const express = require('express');
 const { XMLParser } = require('fast-xml-parser');
+const cron = require('node-cron');
+
 const parser = new XMLParser();
 const app = express();
 const port = 3000;
@@ -12,8 +14,7 @@ app.set('view engine', 'pug');
 // 2. Parser le fichier XML
 // 3. Afficher les données
 
-app.get('/', async (req, res) => {
-    console.log('GET request', new Date());
+async function routine() {
     const xml = await axios.get(
         'https://rss.rtbf.be/article/rss/highlight_rtbf_info.xml?source=internal'
     );
@@ -23,6 +24,11 @@ app.get('/', async (req, res) => {
     const articles = parsed.rss.channel.item;
     console.log('DONE', new Date());
     console.log(title, pubDate, articles.length);
+}
+
+app.get('/', async (req, res) => {
+    console.log('GET request', new Date());
+
     // res.send('Hello');
     /*res.status(200).json({
         title,
@@ -34,6 +40,13 @@ app.get('/', async (req, res) => {
 
 app.listen(port, () => {
     console.log('App is running on port ' + port);
+});
+
+// node-cron
+
+cron.schedule('*/10 * * * * *', () => {
+    console.log('Cron job', new Date());
+    routine();
 });
 
 // app.listen(port, function () {})
